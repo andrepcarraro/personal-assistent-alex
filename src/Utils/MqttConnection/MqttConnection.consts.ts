78796c2ed261577f2dd2f484b48a469890ee704a
André Carraro <andrepcarraro@gmail.com>
2023-05-15 18:34:30 -0300

    Change hive broker credentials

--- src/Utils/MqttConnection/MqttConnection.consts.ts
+++ src/Utils/MqttConnection/MqttConnection.consts.ts
@@ -1,10 +1,10 @@
 import { IClientOptions } from "mqtt";
 
 export const MQTT_OPTIONS: IClientOptions = {
-  username: "RaioIOT", //"hivemq.webclient.1680569923926",
-  password: "Raio-123456789", // "h@8Vp4b3#SYec2W;oQ>E",
+  username: "hivemq.webclient.1684186104777", //"hivemq.webclient.1680569923926",
+  password: "<LP,9MO@*0dpu6kNzH1n", // "h@8Vp4b3#SYec2W;oQ>E",
 };
 
 export const CONNECTION_URL =
-  "wss://c870f335513246ff868ab19f9b93fb12.s2.eu.hivemq.cloud:8884/mqtt";
+  "wss://c2d3f38e6eee4b1597daa92b190781c5.s1.eu.hivemq.cloud:8884/mqtt";
 //"wss://73aa93375c024a12b448ddf523c4d162.s2.eu.hivemq.cloud:8884/mqtt";
